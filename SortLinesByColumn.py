1df86b052e7264aa30ef99b43e6e34762f102e98
Ilya Kulakov <kulakov.ilya@gmail.com>
2013-01-25 20:06:36 +0700

    Allow a user to configure stripping.

--- SortLinesByColumn.py
+++ SortLinesByColumn.py
@@ -8,20 +8,35 @@ class SortLinesByColumnCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, case_sensitive):
         split_regexp = self.settings.get('sortlinesbycolumn_split_regexp', '\W+')
+        should_strip = self.settings.get('sortlinesbycolumn_should_strip', True)
 
         for selection in self.view.sel():
             if(selection is None or selection.empty()):
                 continue
 
+            selection_lines = self.view.lines(selection)
+
+            if len(selection_lines) < 2:
+                continue
+
             last_line_region = self.view.line(selection.end())
             last_word_region = self.view.word(selection.end())
-            last_line_selection_str = self.view.substr(sublime.Region(last_line_region.begin(), last_word_region.end())).strip()
+            last_line_selection_str = self.view.substr(sublime.Region(last_line_region.begin(), last_word_region.end()))
+
+            if should_strip:
+                last_line_selection_str = last_line_selection_str.strip()
+
             column = len(re.split(split_regexp, last_line_selection_str)) - 1
 
             # Turn selected lines into tuple of full line (including new line char) and splitted line using regexp.
             lines = []
-            for line_region in self.view.lines(selection):
-                row = re.split(split_regexp, self.view.substr(line_region).strip())
+            for line_region in selection_lines:
+                line_str = self.view.substr(line_region)
+
+                if should_strip:
+                    line_str = line_str.strip()
+
+                row = re.split(split_regexp, line_str)
 
                 if len(row):
                     lines.append([self.view.substr(self.view.full_line(line_region)), row])
